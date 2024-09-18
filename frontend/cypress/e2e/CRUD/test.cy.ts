01fefc60bf91d87cd738937f934e56cd8b2ba7a2
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-18 18:57:18 +0900

    新しいページを作成した場合のcypressテストケースを作成

--- frontend/cypress/e2e/CRUD/test.cy.ts
+++ frontend/cypress/e2e/CRUD/test.cy.ts
@@ -1,48 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Blog App', () => {
+  const SIDE_BAR_SEL = 'body > div > main > div > div.SideBar_wrapper__RpJdu > ul'
+  const SIDE_BAR_EDIT_BUTTON_SEL = 'body > div > main > div > div.SideBar_wrapper__RpJdu > div > button'
+  const SIDE_BAR_NEW_PAGE_BUTTON_SEL =
+    'body > div > main > div > div.SideBar_wrapper__RpJdu > div > button.Button_button__vMoud.Button_has-icon__vIdOA.Button_is-secondary__Ke1RC.Button_is-create___CIKr'
+  const SIDE_BAR_DONE_BUTTON_SEL =
+    'body > div > main > div > div.SideBar_wrapper__RpJdu > div > button.Button_button__vMoud.Button_has-icon__vIdOA.Button_is-secondary__Ke1RC.Button_is-done__LIc02'
+
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
+    cy.scrollTo('bottom')
   })
 
-  it('displays two todo items by default', () => {
-    cy.get('.todo-list li').should('have.length', 2)
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
-  })
-
-  it('can add new todo items', () => {
-    const newItem = 'Feed the cat'
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
-    cy.get('.todo-list li').should('have.length', 3).last().should('have.text', newItem)
-  })
-
-  it('can check off an item as completed', () => {
-    cy.contains('Pay electric bill').parent().find('input[type=checkbox]').check()
-    cy.contains('Pay electric bill').parents('li').should('have.class', 'completed')
-  })
-
-  context('with a checked task', () => {
-    beforeEach(() => {
-      cy.contains('Pay electric bill').parent().find('input[type=checkbox]').check()
-    })
-
-    it('can filter for uncompleted tasks', () => {
-      cy.contains('Active').click()
-      cy.get('.todo-list li').should('have.length', 1).first().should('have.text', 'Walk the dog')
-      cy.contains('Pay electric bill').should('not.exist')
-    })
-
-    it('can filter for completed tasks', () => {
-      cy.contains('Completed').click()
-      cy.get('.todo-list li').should('have.length', 1).first().should('have.text', 'Pay electric bill')
-      cy.contains('Walk the dog').should('not.exist')
-    })
-
-    it('can delete all completed tasks', () => {
-      cy.contains('Clear completed').click()
-      cy.get('.todo-list li').should('have.length', 1).should('not.have.text', 'Pay electric bill')
-      cy.contains('Clear completed').should('not.exist')
-    })
+  it('ページを追加した際にサイドバーに新しいページが追加されていること', () => {
+    cy.get(SIDE_BAR_EDIT_BUTTON_SEL).click()
+    cy.get(SIDE_BAR_NEW_PAGE_BUTTON_SEL).click()
+    cy.get(SIDE_BAR_DONE_BUTTON_SEL).click()
+    cy.get(SIDE_BAR_SEL).children().last().should('have.text', 'タイトル')
   })
 })
